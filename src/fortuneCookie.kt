d9ec93baab0a0938c97e4142596fba8eb4fca19d
mbukovich <35708097+mbukovich@users.noreply.github.com>
2019-05-28 17:05:57 -0500

    Finished fortune cookie list
    added fortunes to the getFortune function

--- src/fortuneCookie.kt
+++ src/fortuneCookie.kt
@@ -3,5 +3,12 @@ fun main(args : Array<String>) {
 }
 
 fun getFortuneCookie() : String {
-     val fortunes = listOf("You will have a great day.")
+     val fortunes = listOf("You will have a great day!",
+                          "Things will go well for you today.",
+                          "Enjoy a wonderful day of success."
+                          "Be humble and all will be well."
+                          "Today is a good day for exercising restraint."
+                          "Take it easy and enjoy life!"
+                          "Treasure your friends because they are your greatest fortune.")
+     print("Enter your Birthday.")
 }
