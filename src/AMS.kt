4e0458edfd633149568df49d4819c5085951d799
mbukovich <35708097+mbukovich@users.noreply.github.com>
2019-06-05 15:35:59 -0500

    Section 8 of Lesson 3
    Made changes to helloKotlin from section 8 of Lesson 3.

--- src/AMS.kt
+++ src/AMS.kt
@@ -24,11 +24,22 @@ fun dayOfWeek() {
 
 fun feedTheFish() {
     val day = randomDay()
-    val food = "Pellets"
+    val food = fishFood(day)
     println("Today is $day and the fish eat $food")
 }
 
 fun randomDay() : String {
     val week = listOf ("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
     return week[random().nextInt(7)]
+}
+
+fun fishFood (day : String) : String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitos"
+        "Sunday" -> "plankton"
+        else -> "fasting"
+    }
 }
