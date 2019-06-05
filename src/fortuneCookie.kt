61615c006c1fc8e9eabf1ac6e11358e38750a39b
mbukovich <35708097+mbukovich@users.noreply.github.com>
2019-06-05 15:55:25 -0500

    Lesson 3 Section 9
    Modified the fortuneCookie program by adding a getBirthday function and a when statement on the return of the getFortuneCookie function

--- src/fortuneCookie.kt
+++ src/fortuneCookie.kt
@@ -1,14 +1,12 @@
 fun main(args : Array<String>) {
      for (i in 1..10) {
-          var yourFortune = getFortuneCookie()
-          println("Your fortune is: " + yourFortune)
-          if (yourFortune == "Take it easy and enjoy life!") {
-               break
-          }
+          var yourFortune = getFortuneCookie(getBirthday())
+          println("Your fortune is: $yourFortune")
+          if (yourFortune.contains("Take it easy")) break
      }
 }
 
-fun getFortuneCookie() : String {
+fun getFortuneCookie(birthday : Int) : String {
      val fortunes = listOf("You will have a great day!",
                           "Things will go well for you today.",
                           "Enjoy a wonderful day of success."
@@ -16,7 +14,14 @@ fun getFortuneCookie() : String {
                           "Today is a good day for exercising restraint."
                           "Take it easy and enjoy life!"
                           "Treasure your friends because they are your greatest fortune.")
+     return when (birthday) {
+          28 || 31 -> fortunes[4]
+          0..7 -> fortunes[3]
+          else -> fortunes[birthday.rem(fortunes.size)]
+     }
+}
+
+fun getBirthday () : Int {
      print("Enter your Birthday.")
      var birthday = readLine()?.toIntOrNull() ?: 1
-     return fortunes[birthday.rem(fortunes.size)]
 }
