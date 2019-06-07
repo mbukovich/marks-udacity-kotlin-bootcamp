20b9e47c5061c6e4aad1a88fb7897856e7c4cf42
mbukovich <35708097+mbukovich@users.noreply.github.com>
2019-06-07 18:19:36 -0500

    Lesson 3 Section 15
    Finishing the changes for the DoToday program

--- src/DoToday.kt
+++ src/DoToday.kt
@@ -1,20 +1,22 @@
 fun main(args: Array<String>) {
-    
+    println(whatShouldIDoToday())
 }
 
 fun whatShouldIDoToday(
-         mood : String,
          weather : String = "sunny",
          temperature : Int = 24) {
+    print("Please enter your mood: ")
+    var mood : String? = readLine()?: "happy"
+    
     fun isNiceWeather(mood: String, weather: String) = mood == "happy" && weather == "sunny"
     fun isAngry(mood: String) = mood == "angry"
     fun isColdOutside(mood: String, temperature: String) = mood == "happy" && temperature <= 0
     fun isSnowing(weather: String) = weather == "snowing"
     
     return when {
-        isNiceWeather(mood, weather) -> "Go for a walk."
-        isAngry(mood) -> "Go exercise."
-        isColdOutside(mood, temperature) -> "Warm yourself by the fire."
+        isNiceWeather(mood!!, weather) -> "Go for a walk."
+        isAngry(mood!!) -> "Go exercise."
+        isColdOutside(mood!!, temperature) -> "Warm yourself by the fire."
         isSnowing(weather) -> "Make a Snow Man."
         else -> "Stay home and read."
     }
