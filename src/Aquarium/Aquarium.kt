e55bc118d7361c496586f6399a80bd659fb0fb54
mbukovich <35708097+mbukovich@users.noreply.github.com>
2019-06-13 15:56:53 -0500

    Lesson 4 Section 4

--- src/Aquarium/Aquarium.kt
+++ src/Aquarium/Aquarium.kt
@@ -6,5 +6,5 @@ class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40
         get() = return width * height * length / 1000
         set(value) {height = (value * 1000) / (width * length)}
     
-    
+    var water = volume * 0.9
 }
