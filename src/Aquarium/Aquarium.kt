ab6a3c951bf3c2a3e02d55417b69ff57d401dbf8
mbukovich <35708097+mbukovich@users.noreply.github.com>
2019-06-13 17:06:23 -0500

    Lesson 4 Section 4

--- src/Aquarium/Aquarium.kt
+++ src/Aquarium/Aquarium.kt
@@ -7,4 +7,10 @@ class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40
         set(value) {height = (value * 1000) / (width * length)}
     
     var water = volume * 0.9
+    
+    constructor(numberOfFish: Int): this() {
+        val water: Int = numberOfFish * 2000 // cm3
+        val tank: Double = water * water + 0.1
+        height = (tank / (length * width)).toInt()
+    }
 }
