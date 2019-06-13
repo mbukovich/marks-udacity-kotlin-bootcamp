417b8fc7a2b7efc9f4358986c933b2b430a91807
mbukovich <35708097+mbukovich@users.noreply.github.com>
2019-06-13 15:39:25 -0500

    Lesson 4 Section 4

--- src/Aquarium/Aquarium.kt
+++ src/Aquarium/Aquarium.kt
@@ -1,10 +1,6 @@
 package Aquarium
 
-class Aquarium {
-    
-    var width: Int = 20
-    var height: Int = 40
-    var length: Int = 100
+class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40) {
     
     var volume: Int
         get() = return width * height * length / 1000
