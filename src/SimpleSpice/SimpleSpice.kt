372ba31bffbd56d4714164097db0ce369b71aa75
mbukovich <35708097+mbukovich@users.noreply.github.com>
2019-06-13 15:23:53 -0500

    Update SimpleSpice.kt

--- src/SimpleSpice/SimpleSpice.kt
+++ src/SimpleSpice/SimpleSpice.kt
@@ -7,6 +7,10 @@ class SimpleSpice {
     var heat: Int 
         get() = when(spiciness) {
             "mild" -> 5
+            "hot" -> 6
+            "fire" -> 7
+            "blazing" -> 8
+            "scorching" -> 9
             else -> 5
         }
 }
