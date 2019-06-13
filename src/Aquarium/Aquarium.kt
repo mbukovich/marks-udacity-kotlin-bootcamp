7919081cf97406d4e1d600b3f54f6c14b94d0a7f
mbukovich <35708097+mbukovich@users.noreply.github.com>
2019-06-13 14:49:05 -0500

    Update Aquarium.kt

--- src/Aquarium/Aquarium.kt
+++ src/Aquarium/Aquarium.kt
@@ -6,8 +6,9 @@ class Aquarium {
     var height: Int = 40
     var length: Int = 100
     
-    val volume: Int
+    var volume: Int
         get() = return width * height * length / 1000
+        set(value) {height = (value * 1000) / (width * length)}
     
     
 }
