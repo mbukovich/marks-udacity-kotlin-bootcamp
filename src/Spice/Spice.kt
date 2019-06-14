fb42cd385a0d29eecae11c030b73d0b2b486925d
mbukovich <35708097+mbukovich@users.noreply.github.com>
2019-06-14 18:23:58 -0500

    Lesson 4 Section 5

--- src/Spice/Spice.kt
+++ src/Spice/Spice.kt
@@ -1,10 +1,18 @@
 package Spice
 
 class Spice(val name: String, val spiciness: String = "mild") {
+    
+    init {
+        println("Spice Name: $name  Spiciness: $spiciness")
+    }
+    
     var heat: Int
         get() = when(spiciness) {
             "mild" -> 5
+            "spicy" -> 10
+            "flaming" -> 15
             else -> 5
         }
     
+    fun makeSalt() = Spice("Salt", "mild")
 }
