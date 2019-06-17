2aaa6c26423cc87b272120b675e59e4b4d5f9d27
mbukovich <35708097+mbukovich@users.noreply.github.com>
2019-06-17 13:31:55 -0500

    Lesson 4 Section 9
    added eating functionality to the fish

--- src/Aquarium/AquariumFish.kt
+++ src/Aquarium/AquariumFish.kt
@@ -4,10 +4,23 @@ abstract class AquariumFish {
     abstract val color: String
 }
 
-class Shark: AquariumFish() {
+class Shark: AquariumFish(), FishAction {
     override val color = "gray"
+    
+    override fun eat() {
+        println("hunt and eat fish")
+    }
 }
 
-class Plecostomus: AquariumFish() {
+class Plecostomus: AquariumFish(), FishAction {
     override val color = "gold"
+    
+    override fun eat() {
+        println("munch on algae")
+    }
 }
+
+interface FishAction {
+    fun eat()
+}
+
