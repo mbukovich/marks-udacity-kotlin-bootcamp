7d5f108c0de3623bfaf52de28e1d3e4049ef0a85
mbukovich <35708097+mbukovich@users.noreply.github.com>
2019-06-19 19:56:58 -0500

    Lesson 4 Section 10
    Creating the Spices package and Spice abstract class for the assignment in Section 10 of Lesson 4

---
+++ src/Spices/Spices.kt
@@ -0,0 +1,20 @@
+package Spices
+
+abstract class Spice(val name: String, val spiciness: String = "mild") {
+    
+    init {
+        println("Spice Name: $name  Spiciness: $spiciness")
+    }
+    
+    private val heat: Int
+        get() = when(spiciness) {
+            "mild" -> 5
+            "spicy" -> 10
+            "flaming" -> 15
+            else -> 5
+        }
+    
+    fun makeSalt() = Spice("Salt", "mild")
+}
+
+class Curry: Spice() {}
