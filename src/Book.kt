4e4b891e7354060cba4c3edbb13092ef511f97cc
mbukovich <35708097+mbukovich@users.noreply.github.com>
2019-06-20 19:58:42 -0500

    Lesson 5 Section 5
    Added some collections for the assignment in section 5 of Lesson 5

--- src/Book.kt
+++ src/Book.kt
@@ -3,6 +3,15 @@ fun main(args: Array<String>) {
     val (title, author, year) = foundation.getTitleAuthorYear()
     
     println("$author published the book $title in the year $year.")
+    
+    val allBooks = setOf("Romeo and Juliet", "Macbeth", "Taming of the Shrew", "Hamlet")
+    
+    val library = mapOf("Shakespeare" to allBooks)
+    
+    println(library.any {it.value.contains("Hamlet")})
+    
+    val moreBooks = mutableMapOf<String, String>("Great Expectations" to "Dickens")
+    moreBooks.getOrPut("A Tale of Two Cities") {"Dickens"}
 }
 
 class Book(val title: String, val author: String, val year: String) {
