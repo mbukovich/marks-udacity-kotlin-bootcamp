8f5292c33a888a5287f41f618116db87e82ac9e3
mbukovich <35708097+mbukovich@users.noreply.github.com>
2019-06-24 16:18:20 -0500

    Lesson 5 Section 16
    Added a generic function to the class BaseBuildingMaterial for the assignment in section 16 of Lesson 5

--- src/Buildings/BaseBuildingMaterial.kt
+++ src/Buildings/BaseBuildingMaterial.kt
@@ -2,4 +2,12 @@ package Buildings
 
 open class BaseBuildingMaterial() {
     open val numberNeeded = 1
+    
+    fun <T: BaseBuildingMaterial> isSmallBuilding (building: Building<T>) {
+        if (building.numberNeeded < 500) {
+            println("Small Building")
+        } else {
+            println("Large Building")
+        }
+    }
 }
